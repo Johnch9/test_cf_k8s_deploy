b9e56ca7dbe251f4cdc30c69aa0a3643e59ed90e
Howard <P95048490@capita.co.uk>
2018-11-19 09:25:46 +0000

    Added rbac

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "azurerm" {
-  version = ">= 1.18.0"
+  version = ">= 1.19.0"
   client_id = "${var.client_id}"
   client_secret = "${var.client_secret}"
   tenant_id = "${var.tenant_id}"
