fa44c8b8529014daaf920ac759c2a87f12720228
Howard <P95048490@capita.co.uk>
2018-11-19 09:43:10 +0000

    Added enable_rbac flag

--- terraform/az_k8s.tf
+++ terraform/az_k8s.tf
@@ -3,6 +3,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     location            = "${azurerm_resource_group.k8s.location}"
     resource_group_name = "${azurerm_resource_group.k8s.name}"
     dns_prefix          = "${var.dns_prefix}"
+    enable_rbac         = true
 
     # linux_profile {
     #     admin_username = "ubuntu"
@@ -23,9 +24,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     service_principal {
         client_id     = "${var.client_id}"
         client_secret = "${var.client_secret}"
-    }
-
-    role_based_access_control {
     }
 
     tags {
