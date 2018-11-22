a186b8ff9ce54dc4d832c7535869d2700966c588
Howard <P95048490@capita.co.uk>
2018-11-22 13:20:30 +0000

    Added service principal

--- scripts/az_setup.sh
+++ scripts/az_setup.sh
@@ -26,7 +26,8 @@ until [ $N -ge 3 ]; do
     --resource-group "${RESOURCE_GROUP}" \
     --name "${CLUSTER_NAME}" \
     --node-count ${NODE_COUNT} \
-    --generate-ssh-keys
+    --service-principal ${CLIENT_ID} \ 
+    --client-secret ${CLIENT_SECRET}
   if [[ "$?" == "0" ]]; then
     SUCCESS="true"
     break
